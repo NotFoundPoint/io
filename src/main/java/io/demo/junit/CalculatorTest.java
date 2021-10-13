f4bc0c1e64cbcb87d1cade912a77bce0d9627eab
dxp <dxp@dxp.com>
2021-10-13 15:34:05 +0800

    master third commit

--- src/main/java/io/demo/junit/CalculatorTest.java
+++ src/main/java/io/demo/junit/CalculatorTest.java
@@ -6,6 +6,7 @@ public class CalculatorTest {
 
     @Test
     public void addTest(){
+        System.out.println("This is master");
         System.out.println("fix hot");
         System.out.println("Welcome use Calculate");
         Calculator calculator = new Calculator();
